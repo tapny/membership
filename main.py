56dcc466a180eec9a274da1bacb92345d166d8a5
victor <vjwang45@gmail.com>
2014-05-31 16:21:31 -0400

    get emails from new subscribers function

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
+import os
+import eventbrite
+import csv
 
 
+EB_APP_KEY = os.environ['EB_APP_KEY']
+EB_API_KEY = os.environ['EB_API_KEY']
 
+FOR_YEAR = 2014
 
-
-
-
-EB_APP_KEY = ""
-EB_API_KEY = ""
-
+eid = 11666856883
 
 eb_auth_tokens = {'app_key':  EB_APP_KEY, 'user_key':EB_API_KEY}
 eb_client = eventbrite.EventbriteClient(eb_auth_tokens)
 
+attendees = eb_client.event_list_attendees({"id":11666856883})['attendees']
+
+
+#[0]['attendee']['answers'][0]['answer']['answer_text']
+
+def get_new_subscribers(event_id)
+    # only grab emails for those who said "yes" to subscribing to newsletter
+    attendees = eb_client.event_list_attendees({"id":event_id})['attendees']
+    subscribers = []
+    for each in attendees:
+        attendee = each['attendee']
+        if attendee['answers'] and attendee['answers'][0]['answer']['answer_text'].lower() == "yes":
+            first = attendee['first_name']
+            last = attendee['last_name']
+            email = attendee['email']
+            subscribers.append( (first, last, email) )
+    return subscribers
+
+def write_to_csv(filename, writelist):
+    with open(filename, 'wb') as csvfile:
+        writer = csv.writer(csvfile)
+
+if __name__ == "__main__":
+    
