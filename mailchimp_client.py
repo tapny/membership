40ccb8c4812630f7dc4297edd69dae9534f859ed
ribrab <tjrtsai@gmail.com>
2014-06-10 10:36:16 -0400

    Completed MailChimp client

--- mailchimp_client.py
+++ mailchimp_client.py
@@ -5,19 +5,16 @@ MC_API_KEY = os.environ['MC_API_KEY']
 mc_client = mailchimp.Mailchimp(MC_API_KEY)
 
 def addSubscribers(list_id, subscribers):
-    print 1
+    return mc_client.lists.batch_subscribe(list_id, subscribers, double_optin=False)
 
 if __name__ == "__main__":
-    lists = mc_client.lists.list()
+    pass
 
-    
+##    lists = mc_client.lists.list()
 ##    for l in lists["data"]:
 ##        if l["name"] == "TEST List":
 ##            list_id = l["id"]
 ##            break
-
-    list_id = '8196fa6dba'
-    subscribers = [{"email": {"email": "tjrtsai@gmail.com"}, "merge_vars": {"FNAME": "Ray", "LNAME": "Tsai"}}]
-    result = mc_client.lists.batch_subscribe(list_id, subscribers, double_optin=False)
-    print result
+##    subscribers = [{"email": {"email": "tjrtsai@gmail.com"}, "merge_vars": {"FNAME": "Ray", "LNAME": "Tsai"}}]
+    
     
